5830f457761d42114cd152bab15dd1bedfab9d42
robbyvan <fzpinus@gmail.com>
2016-11-01 15:55:32 -0700

    updated echo
    added while loop so the server keep listening and accept request from
    client

--- echo_client.cpp
+++ echo_client.cpp
@@ -5,40 +5,58 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <signal.h>
+
+#define BUF_SIZE 100
+
 void DieWithError(char *errorMessage) {
   perror(errorMessage);
   exit(1);
 }
 
 int main(){
-  int sock = socket(AF_INET, SOCK_STREAM, 0);
-  if (sock < 0)
-  {
-    DieWithError("socket() failed!");
-  }
+  
 
   struct sockaddr_in serv_addr;
   memset(&serv_addr, 0, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  serv_addr.sin_port = htons(1235);
-  if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){
-    DieWithError("connect() failed!");
+  serv_addr.sin_port = htons(1234);
+
+  char bufferSend[BUF_SIZE] = {'\0'};
+  char bufferRecv[BUF_SIZE] = {'\0'};
+
+  while (1){
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
+      DieWithError("socket() failed!");
+    }
+
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){
+      DieWithError("connect() failed!");
+    }
+
+    printf("Input a string to send: ");
+    gets(bufferSend);
+
+
+    if (send(sock, bufferSend, strlen(bufferSend), 0) < 0){
+      DieWithError("Send() failed!");
+    }
+
+    if (bufferSend[0] == 'Q'){
+      close(sock);
+      break;
+    }
+
+    recv(sock, bufferRecv, BUF_SIZE, 0);
+
+    printf("messages from server: %s\n", bufferRecv);
+
+    memset(bufferSend, 0, BUF_SIZE);
+    memset(bufferRecv, 0, BUF_SIZE);
+    close(sock);
   }
-
-  char bufferSend[30] = {'\0'};
-  printf("Input a string to send: ");
-  scanf("%s", bufferSend);
-  if (send(sock, bufferSend, strlen(bufferSend), 0) < 0){
-    DieWithError("Send() failed!");
-  }
-
-  char buffer[40] = {'\0'};
-  recv(sock, buffer, sizeof(buffer)-1, 0);
-
-  printf("messages from server: %s\n", buffer);
-
-  close(sock);
 
   return 0;
 }
