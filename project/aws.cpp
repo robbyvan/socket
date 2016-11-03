fe2501387f2360033087cab62be274f9e3b6da89
robbyvan <fzpinus@gmail.com>
2016-11-02 22:30:35 -0700

    finished transit file between AWS and Client

--- project/aws.cpp
+++ project/aws.cpp
@@ -14,29 +14,50 @@ USC ID: 69515-21807
 #define BUF_SIZE 100
 
 int main(){
+  //用来握手的socket
   int tcp_origin_sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 
   struct sockaddr_in server_addr;
   memset(&server_addr, 0, sizeof(server_addr));
   server_addr.sin_family = PF_INET;
   server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");//
-  server_addr.sin_port = htons("25807");
-  bind(server_addr, (struct sockaddr*)&server_addr, sizeof(server_addr));
+  server_addr.sin_port = htons(25807);
+  bind(tcp_origin_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
 
   if ( listen(tcp_origin_sock, 50) > -1 ){
     printf("The AWS is up and running.\n");
   }
 
-  struct sockaddr_in client_addr;
-  socklen_t client_addr_size = sizeof(client_addr);
-  char buffer[BUF_SIZE] = {'\0'};
-
+  int countClient = 0;
+  char *clientFin = "clientFin";
+  FILE *fpRecvNum = fopen("./redvNum.csv", "wb");
   while(1){
+    //accept后新建一个交换数据的client_sock
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_size = sizeof(client_addr);
     int client_sock = accept(tcp_origin_sock, (struct sockaddr*)&client_addr, &client_addr_size);
+    countClient++;
 
-    int buffer_len = recv(client_sock, buffer, BUF_SIZE, 0);
-    
+    //服务器接受client来的数据的buffer
+    char bufRecvFromClnt[BUF_SIZE] = {'\0'};
+    // char fileBuffer[BUF_SIZE] = {'\0'};
+    int buffer_len = recv(client_sock, bufRecvFromClnt, BUF_SIZE, 0);
 
+    if (strcmp(bufRecvFromClnt, clientFin) == 0){
+      countClient -= 1;
+      printf("The AWS has received %d numbers from the client using TCP over port 25807\n", countClient);
+      break;
+    }
 
+    if (buffer_len > 0){
+      fprintf(fpRecvNum, "%s\n", bufRecvFromClnt);
+    }
+
+    close(client_sock);
+    memset(bufRecvFromClnt, 0, BUF_SIZE);
   }
+
+  close(tcp_origin_sock);
+  printf("tcp_origin_sock closed\n");
+  return 0;
 }
--- project/client.cpp
+++ project/client.cpp
@@ -1,1 +1,53 @@
-client.cpp
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <signal.h>
+
+#define BUF_SIZE 100
+
+int main(){
+
+  struct sockaddr_in serv_addr;
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family = PF_INET;
+  serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  serv_addr.sin_port = htons(25807);
+
+  char bufSendToServ[BUF_SIZE] = {'\0'};
+  char bufRecvFromServ[BUF_SIZE] = {'\0'};
+
+  FILE *fp = fopen("./nums.csv", "rb");
+  if (fp == NULL){
+    printf("Can not open file\n");
+  }else{
+    printf("Open Success.\n");
+  }
+  
+  while(!feof(fp)){
+    fscanf(fp, "%s\n", &bufSendToServ);
+    
+    int sock = socket(PF_INET, SOCK_STREAM, 0);
+    connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
+
+    send(sock, bufSendToServ, strlen(bufSendToServ), 0);
+    memset(bufSendToServ, 0, BUF_SIZE);
+    close(sock);//发送数字结束
+  }
+
+  char finMsg[] = "clientFin";
+  strcpy(bufSendToServ, finMsg);
+  
+  int sock = socket(PF_INET, SOCK_STREAM, 0);
+  connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
+  
+  send(sock, bufSendToServ, strlen(bufSendToServ), 0);
+  printf("%s\n", finMsg);
+  
+  memset(bufSendToServ, 0, BUF_SIZE);
+  shutdown(sock, SHUT_WR);
+
+  return 0;
+}
